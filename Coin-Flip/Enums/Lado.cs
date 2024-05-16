7f29e8fcaec84decbcd9b641f51740daf1161933
Matheus Pinter <matheuspinter08@gmail.com>
2024-05-16 12:34:36 -0300

    feat: Adicionei MVVM ao projeto

---
+++ Coin-Flip/Enums/Lado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coin_Flip.Enums
+{
+    internal class Lado
+    {
+    }
+}
---
+++ Coin-Flip/Models/Coin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coin_Flip.Models
+{
+    internal class Coin
+    {
+    }
+}
